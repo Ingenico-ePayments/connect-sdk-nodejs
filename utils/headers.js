a2b2006eff961789c7bb938e9e4c19caa6f71ebf
Ingenico ePayments <jenkins@isaac.nl>
2018-09-05 10:59:53 +0200

    Release 2.21.0.

--- utils/headers.js
+++ utils/headers.js
@@ -9,7 +9,7 @@ var serverMetaInfo = function (sdkContext) {
     key: "X-GCS-ServerMetaInfo",
     value: {
       'sdkCreator': 'Ingenico',
-      'sdkIdentifier': 'NodejsServerSDK/v2.20.0',
+      'sdkIdentifier': 'NodejsServerSDK/v2.21.0',
       'platformIdentifier': process.env['OS'] + ' Node.js/' + process.versions.node
     }
   };
