1ad80cf6f20e35f0e51ec877a21ffecadb254389
Ingenico ePayments <github@epay.ingenico.com>
2021-11-23 13:11:13 +0100

    Release 3.20.0.

--- utils/headers.js
+++ utils/headers.js
@@ -9,7 +9,7 @@ var serverMetaInfo = function (sdkContext) {
     key: "X-GCS-ServerMetaInfo",
     value: {
       'sdkCreator': 'Ingenico',
-      'sdkIdentifier': 'NodejsServerSDK/v3.19.0',
+      'sdkIdentifier': 'NodejsServerSDK/v3.20.0',
       'platformIdentifier': process.env['OS'] + ' Node.js/' + process.versions.node
     }
   };
